a5064f0e90d66e138d7f7369cc567b741d1882e5
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-01-24 19:48:07 +0100

    add control

--- js/main.js
+++ js/main.js
@@ -9,23 +9,27 @@ const prezzoEuroKm = 0.21;
 let price = km * prezzoEuroKm;
 
 // MINORENNE
+if (!isNaN(età) && !isNaN(km)){
+    if(età < 18) {
+        const scontoMinorennePerc = 20;
+        const scontoMinorenneEuro = price * 0.2;
+        const pricef = price - scontoMinorenneEuro;
 
-if(età < 18) {
-    const scontoMinorennePerc = 20;
-    const scontoMinorenneEuro = price * 0.2;
-    const pricef = price - scontoMinorenneEuro;
+        console.log(`Prezzo under 18 ${pricef}`);
+    }
+    // ANZIANO
 
-    console.log(`Prezzo under 18 ${pricef}`)
-}
+    if (età >= 65) {
+        const scontoAnzianoPerc = 40;
+        const scontoAnzianoEuro = price * 0.4;
+        const pricef = price - scontoAnzianoEuro;
 
-if (età >= 65) {
-    const scontoAnzianoPerc = 40;
-    const scontoAnzianoEuro = price * 0.4;
-    const pricef = price - scontoAnzianoEuro;
+        console.log(`Prezzo over 65 ${pricef}`);
+    }
 
-    console.log(`Prezzo over 65 ${pricef}`)
-}
-
-if (età >= 18 && età < 65 ) {
-    console.log(`Prezzo ${price}`)
+    if (età >= 18 && età < 65 ) {
+        console.log(`Prezzo ${price}`);
+    }
+} else {
+    console.log('Valori invalidi');
 }
